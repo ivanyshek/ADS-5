91730da0b02c641544cbc0fe4dd44191e77ef30e
ivanyshek <105326502+ivanyshek@users.noreply.github.com>
2024-04-05 21:30:43 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,38 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+	T data[size];
+	int top;
+public:
+	TStack() :top(0) {}
+	void push(const T& item) {
+		if (top <= size - 1) {
+			data[top++] = item;
+		}
+		else {
+			throw std::string("full");
+		}
+	}
+	T pop() {
+		if (top > 0) {
+			return data[--top];
+		}
+		else {
+			throw std::string("Empty");
+		}
+	}
+	bool isEmpty()const{
+		return top == 0;
+	}
+	T get() const {
+		if (top > 0) {
+			return data[top - 1];
+		}
+		else {
+			throw std::string("Empty");
+		}
+	}
 };
 
 #endif  // INCLUDE_TSTACK_H_
